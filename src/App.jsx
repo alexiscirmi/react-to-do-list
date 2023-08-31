af2e0cc54db42bb6d30c3095c06eb390727e4cbd
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-08-31 19:38:01 -0300

    UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import Input from './components/Input/Input';
 import Task from './components/Task/Task';
+import { ToastContainer, toast } from 'react-toastify';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
   const addTask = newTask => {
     if (!list.some((task) => task.description === newTask.description)) {
       setList([newTask, ...list]);
+    } else {
+      toast.error('That task is already added');
     }
   };
 
@@ -21,7 +25,11 @@ function App() {
   return (
     <div className='App'>
       <main>
-        <h1>TO DO LIST</h1>
+        <section className='d-flex justify-content-center gap-2'>
+          <span className='simple'>(So simple)</span>
+          <h1>TO DO LIST</h1>
+        </section>
+
         <div className='container'>
 
           <Input
@@ -42,6 +50,19 @@ function App() {
         </div>
       </main>
 
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+
       <footer className='m-2 fs-5'>
         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-c-circle' viewBox='0 0 16 16'>
           <path d='M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512Z' />
@@ -49,7 +70,7 @@ function App() {
 
         <span className='ms-1'><a href='https://github.com/cirmialexis/react-to-do-list'>Alexis Cirmi</a></span>
       </footer>
-    </div>
+    </div >
   );
 };
 
